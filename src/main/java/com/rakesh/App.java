7a6bf94b5c8ee0ec9a104570832f4e804ee6bba1
Rakesh <Rakesh@gmail.com>
2026-09-16 09:30:53 +0000

    Initial Java Maven project

---
+++ src/main/java/com/rakesh/App.java
@@ -0,0 +1,9 @@
+package com.rakesh;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Rakesh Java Application!");
+        System.out.println("This application is packaged as a JAR file.");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
